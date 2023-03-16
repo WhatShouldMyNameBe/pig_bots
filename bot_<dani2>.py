b30f6ca8080ac3b09988e68225053d547f57a2d8
Tsao, Dani <87443159+WhatShouldMyNameBe@users.noreply.github.com>
2023-03-16 13:42:38 -0700

    bot_<dani2>.py

---
+++ bot_<dani2>.py
@@ -0,0 +1,8 @@
+def choice(round_score, my_score, opponent_score):
+  if my_score + round_score > 99:
+    return False
+  else:
+      if score < 30:
+          return True
+      else:
+          return False
